6a0eebd56d18d7666c78946133799695ebab4ccd
Mayankkarayat <mayank64641karayat@gmail.com>
2024-02-15 10:19:48 +0530

    Added Responsiveness

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-let slide = document.querySelector('.slide');
+let slide = document.querySelectorAll('.slide');
+let first = document.querySelector('.first')
+let second = document.querySelector('.second')
+let third = document.querySelector('.third')
 let image = document.querySelectorAll('.image');
 let previous = document.querySelector('.pre')
 let next = document.querySelector('.next')
 let slidernumber = 1;
 let length = image.length;
 
+slide.forEach((e)=>{
+    console.log(e);
+    
+})
+
+
 setInterval(()=>{
     if(slidernumber<length){
-        slide.style.transform = `translateX(-${slidernumber*100}%)`;
+        slide[slidernumber].style.transform = `translateX(-${slidernumber*100}%)`;
         slidernumber++;
     }
-    else{
-        slide.style.transform = `translateX(0)`;
-        slidernumber = 1;
-    }
+    // else if(slidernumber>length)
+    // {
+    //     console.log(slidernumber);
+    //     slide[slidernumber-1].style.transform = `translateX(0%)`;
+    //     third.style.transform = `translateX(0%)`;
+    //     second.style.transform = `translateX(0%)`;
+    //     first.style.transform = `translateX(0%)`;
+    //     slidernumber = 1;
+    //     console.log(slidernumber);
+    //     slidernumber=1;
+    // }
 },5000)
 
 next.addEventListener('click',()=>{
     if(slidernumber<length){
-        slide.style.transform = `translateX(-${slidernumber*100}%)`;
+        slide[slidernumber].style.transform = `translateX(-${slidernumber*100}%)`;
         slidernumber++;
     }
     else{
-        slide.style.transform = `translateX(0)`;
+        slide[slidernumber].style.transform = `translateX(0)`;
         slidernumber = 1;
     }
 })
 
 previous.addEventListener('click',()=>{
     if(slidernumber>1){
-        slide.style.transform = `translateX(-${(slidernumber-2)*100}%)`;
+        console.log(slidernumber);
+        slide[slidernumber-1].style.transform = `translateX(-${(slidernumber-2)*100}%)`;
         slidernumber--;
     }
     else{
-        slide.style.transform = `translateX(-${(length-1)*100}%)`;
+        slide[slidernumber].style.transform = `translateX(-${(length-1)*100}%)`;
         slidernumber = length;
     }
 })
@@ -44,15 +61,14 @@ let menu = document.querySelector('.menu');
 let check = 0;
 
 menu.addEventListener("click",()=>{
-    console.log('hello');
     if(check==0){
-        humburger.style.display = "flex";
+        humburger.style.top = "0";
         check=1;
     }
     else{
-        humburger.style.display = "none";
+        humburger.style.top = "-200%";
         check=0;
     }
-    humburger.classList.toggle('active');
+    // humburger.classList.toggle('active');
 })
 
