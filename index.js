0d4b432cfcc7b04c74eaf2ddbc1e9f9ed3ca185c
Mayankkarayat <mayank64641karayat@gmail.com>
2024-02-21 23:03:04 +0530

    Added Half Suit Section

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let slide = document.querySelectorAll('.slide');
 let first = document.querySelector('.first')
 let second = document.querySelector('.second')
 let third = document.querySelector('.third')
+let fourth = document.querySelector('.forth')
 let image = document.querySelectorAll('.image');
 let previous = document.querySelector('.pre')
 let next = document.querySelector('.next')
@@ -19,6 +20,7 @@ setInterval(()=>{
     {
         console.log(slidernumber);
         slide[slidernumber-1].style.transform = `translateX(0%)`;
+        fourth.style.transform = `translateX(0%)`;
         third.style.transform = `translateX(0%)`;
         second.style.transform = `translateX(0%)`;
         first.style.transform = `translateX(0%)`;
